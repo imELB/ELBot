691850c5fe693bfdb9eaae12a5e2ed44fd4c1c79
ELB <aaaa.elb@gmail.com>
2026-02-13 17:53:54 -0300

    main
    comit dos comandos, descobri q tem outro método

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import discord
 
+'''
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print('\nLogged on as', self.user)
 
     async def on_message(self, message):
-
         # Confere se a mensagem é de si mesmo
         if message.author == self.user:
             return
-
         #ping pong básico
         if message.content == 'ping':
             await message.channel.send('pong')
+            return
+'''
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True # para ler o chat
+# futuramente aprender a usar comando de barra
 client = MyClient(intents=intents)
 
 # Token do bot

fe3530b4338f7cc10912eb9831a269d5de72edf3
ELB <aaaa.elb@gmail.com>
2026-02-13 22:21:52 -0300

    Refactor Discord client and implement commands
    Refactor Discord bot client and add slash commands.

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 import discord
+from discord import app_commands
 
-'''
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('\nLogged on as', self.user)
+guild = discord.Object(id=1362952956684668988)
 
-    async def on_message(self, message):
-        # Confere se a mensagem é de si mesmo
-        if message.author == self.user:
-            return
-        #ping pong básico
-        if message.content == 'ping':
-            await message.channel.send('pong')
-            return
-'''
+class dependencias(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
 
-intents = discord.Intents.default()
-intents.message_content = True # para ler o chat
-# futuramente aprender a usar comando de barra
-client = MyClient(intents=intents)
+    async def setup_hook(self):
+        await self.tree.sync(guild=guild)
+        print('\n Comandos sincronizados\n')
 
+client = dependencias()
+
+
+# evento onready
+@client.event
+async def on_ready():
+    print(f'\nLogged on as {client.user}')
+
+
+# comandos direto no chat
+@client.event
+async def on_message(message): 
+    if message.author == client.user:
+        return
+    if message.content == '.ping':
+        await message.channel.send('pong')
+        return
+
+
+# comandos de barra daqui pra baixo
+@client.tree.command(name="ping", description="Responde com pong!", guild=guild)
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
+    return
+
+@client.tree.command(name="abrido", description="Comando secreto dos enclavos", guild=guild)
+async def abrido(interaction: discord.Interaction):
+    await interaction.response.send_message("Idoso")
+    return
+
+@client.tree.command(name="teste", description="afbjkbjk", guild=guild)
+async def teste(interaction: discord.Interaction):
+    await interaction.response.send_message(f'{interaction.user.mention} teste')
+    return
 # Token do bot
 client.run('token')
